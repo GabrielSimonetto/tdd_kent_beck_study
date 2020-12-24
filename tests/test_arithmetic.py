1cb45dbc00a1c2a74ca54eb3d95268b076069dd1
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2020-12-24 13:40:50 -0300

    Added pytest version of multiplication test.

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -1,6 +1,20 @@
+import pytest
 from money.dollar import Dollar
 
-def test_multiplication():
+@pytest.mark.parametrize(
+    "amount, multiplier, expected",
+    [
+        (5, 2, 10),
+        (5, 3, 15),
+    ]
+)
+def test_multiplication_pytest_version(amount, multiplier, expected):
+    dollar = Dollar(amount)
+    product = dollar.times(multiplier)
+    assert expected == product.amount
+
+
+def test_multiplication_book_version():
     amount = 5
 
     multiplier = 2
