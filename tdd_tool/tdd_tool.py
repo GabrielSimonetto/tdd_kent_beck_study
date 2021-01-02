7f97a914fc0a733751b354b64327968ea073fe84
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2021-01-02 13:34:05 -0300

    Chapter 23: implementing summary()

--- tdd_tool/tdd_tool.py
+++ tdd_tool/tdd_tool.py
@@ -8,10 +8,13 @@ class TestCase(ABC):
         self.log = ""
 
     def run(self):
+        result = TestResult()
+        result.test_started()
         self.set_up()
         exec(f"self.{self.name}()")
         print(f"{self.name} is working")
         self.teardown()
+        return result
 
     # @abstractmethod
     # ainda nao funciona, WasRun eh muito diferente de TestCaseTest
@@ -21,6 +24,17 @@ class TestCase(ABC):
     # @abstractmethod
     def teardown(self):
         pass
+
+
+class TestResult():
+    def __init__(self):
+        self.run_count = 0
+
+    def test_started(self):
+        self.run_count += 1
+
+    def summary(self):
+        return f"{self.run_count} run, 0 failed"
 
 
 class WasRun(TestCase):
@@ -33,6 +47,11 @@ class WasRun(TestCase):
     def test_method(self):
         self.was_run = True
         self.log += "test_method "
-    
+
+    # we aren't catching this exception!
+    # this makes it so our tests halt instead of carrying on.
+    def test_broken_method(self):
+        raise Exception
+
     def teardown(self):
         self.log += "teardown "
--- tdd_tool/tests.py
+++ tdd_tool/tests.py
@@ -9,15 +9,26 @@ class TestCaseTest(TestCase):
     #     assert self.test.was_run
 
     def test_template_method(self):
-        self.test = WasRun("test_method")
-        self.test.run()
-        assert self.test.log == "set_up test_method teardown ", (
-            f"this log contained: {self.test.log}"
+        test = WasRun("test_method")
+        test.run()
+        assert test.log == "set_up test_method teardown ", (
+            f"this log contained: {test.log}"
         )
 
+    def test_result(self):
+        test = WasRun("test_method")
+        result = test.run()
+        assert "1 run, 0 failed" == result.summary()
+
+    def test_failed_result(self):
+        test = WasRun("test_broken_method")
+        result = test.run()
+        assert "1 run, 1 failed" == result.summary()
 
 
 TestCaseTest("test_template_method").run()
+TestCaseTest("test_result").run()
+TestCaseTest("test_failed_result").run()
 # TestCaseTest("test_running").run()
 # TestCaseTest("test_set_up").run()
 
