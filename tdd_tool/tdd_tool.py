56bed117bd5fd136f04e591f8ef86d7d334f5b32
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2021-01-02 13:19:08 -0300

    Chapter 22: implement teardown

--- tdd_tool/tdd_tool.py
+++ tdd_tool/tdd_tool.py
@@ -1,21 +1,38 @@
-class TestCase():
+from abc import ABC, abstractmethod
+
+class TestCase(ABC):
     def __init__(self, name):
         self.was_run = None
         self.name = name
         self.was_set_up = False
+        self.log = ""
 
     def run(self):
         self.set_up()
         exec(f"self.{self.name}()")
         print(f"{self.name} is working")
+        self.teardown()
 
-    # colocar abstract?
+    # @abstractmethod
+    # ainda nao funciona, WasRun eh muito diferente de TestCaseTest
     def set_up(self):
         pass
 
+    # @abstractmethod
+    def teardown(self):
+        pass
+
+
 class WasRun(TestCase):
-    def test_method(self):
-        self.was_run = True
 
     def set_up(self):
+        self.was_run = False
         self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+    
+    def teardown(self):
+        self.log += "teardown "
--- tdd_tool/tests.py
+++ tdd_tool/tests.py
@@ -1,19 +1,23 @@
 from tdd_tool import TestCase, WasRun
 
 class TestCaseTest(TestCase):
-    def set_up(self):
+    # def set_up(self):
+    #     self.test = WasRun("test_method")
+
+    # def test_running(self):
+    #     self.test.run()
+    #     assert self.test.was_run
+
+    def test_template_method(self):
         self.test = WasRun("test_method")
-
-    def test_running(self):
         self.test.run()
-        assert self.test.was_run
-
-    def test_set_up(self):
-        self.test.run()
-        assert self.test.was_set_up
+        assert self.test.log == "set_up test_method teardown ", (
+            f"this log contained: {self.test.log}"
+        )
 
 
 
-TestCaseTest("test_running").run()
-TestCaseTest("test_set_up").run()
+TestCaseTest("test_template_method").run()
+# TestCaseTest("test_running").run()
+# TestCaseTest("test_set_up").run()
 
